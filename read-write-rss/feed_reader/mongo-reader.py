901e521cd9e7942b640c4ee12a473235a6645f22
Rosario Laface <rosario.laface@nttdata.com>
2019-10-03 14:56:29 +0200

    Update mongo-reader.py

--- read-write-rss/feed_reader/mongo-reader.py
+++ read-write-rss/feed_reader/mongo-reader.py
@@ -1,8 +1,7 @@
 '''
 this function read rss content from mongo DB and call for AI model REST API
 '''
-
-import pymongo
+import feedparser
 import pymongo
 import pprint
 import requests
@@ -10,12 +9,24 @@ import re
 from datetime import date
 import telepot
 import os
+import json
 
-TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
-bot = telepot.Bot(TELEGRAM_TOKEN)
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN'] #'826514544:AAH_yj9x0CD6auL-N49XGFRi7JqavhrJnaE'
+TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID'] #'-1001457839912'
 
-def send_telegram(msg): #see https://ludusrusso.cc/2017/04/27/implementiamo-un-bot-telegram-con-python/
-    bot.sendMessage(msg)
+def send_message(test_url):
+    params = {
+        'chat_id': TELEGRAM_CHAT_ID,
+        'text': test_url
+    }
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    r = requests.get(url, params=params)
+    if r.status_code == 200:
+        print(json.dumps(r.json(), indent=2))
+    else:
+        r.raise_for_status()
+        
+send_message(test_url)
 
 def send_data_to_ai(documents):
      for content in documents:
@@ -44,26 +55,17 @@ lines = f.readlines()
 contents=[]
 #Parsing feed rss
 for url in lines:
-    feed = feedparser.parse(url) #TODO add more feeds to parse
+    feed = feedparser.parse(url) 
     feed_name=feed['feed']['title']
     mydocs = db[feed_name].find({'date': {'$lt': end, '$gt': start}})
-    if re.match(r'^TechCrunch', feed_name):
+    if re.match(r'^TechCrunch', feed_name): 
         for i in range(0, mydocs.count()):
-            contents.append(mydocs[i]['link'])
-    if re.match(r'^DZone ', feed_name):
+            contents.append(mydocs[i]['content'][0]['value'], mydocs[i]['link'])
+    else:
         for i in range(0, mydocs.count()):
-            contents.append(mydocs[i]['link']) #TODO: test link saving
-    if re.match(r'^Entrepreneur', feed_name):
-        for i in range(0, mydocs.count()):
-            contents.append(mydocs[i]['link'])
-    if re.match(r'^Hacker Noon', feed_name):
-        for i in range(0, mydocs.count()):
-            contents.append(mydocs[i]['link'])
-    if re.match(r'^Wired', feed_name):
-        for i in range(0, mydocs.count()):
-            contents.append(mydocs[i]['link'])
+            contents.append(mydocs[i]['content'], mydocs[i]['link']) 
 
 for content in contents:
-    if send_data_to_ai(content['value']):
-        send_telegram(content['link']) 
+    if send_data_to_ai(content[0]):
+        send_message(content[1]) 
 
