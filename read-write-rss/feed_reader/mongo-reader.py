b32a46e7d79ecdbdfc8f87119797ca4f56a61913
riolaf05 <30504264+riolaf05@users.noreply.github.com>
2019-11-08 20:20:58 +0100

    Update mongo-reader.py

--- read-write-rss/feed_reader/mongo-reader.py
+++ read-write-rss/feed_reader/mongo-reader.py
@@ -7,40 +7,41 @@ import pprint
 import requests
 import re
 from datetime import date
+import datetime
 import telepot
 import os
 import json
 
-TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN'] 
-TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID'] 
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
 
-def send_message(test_url):
+def send_message(text):
     params = {
         'chat_id': TELEGRAM_CHAT_ID,
-        'text': test_url
+        'text': text
     }
-    url = "https://api.telegram.org/bot{}/sendMessage".format(TELEGRAM_BOT_TOKEN)
+    url = "https://api.telegram.org/bot{}/sendMessage".format(TELEGRAM_TOKEN)
     r = requests.get(url, params=params)
     if r.status_code == 200:
         print(json.dumps(r.json(), indent=2))
     else:
         r.raise_for_status()
-        
+
 def send_data_to_ai(content):
-        r = requests.post('http://localhost:5000/predict?input={}'.format(content)) 
+        r = requests.post('http://0.0.0.0:5000/predict?input={}'.format(content))
         return r.json()['predictions'][0]
 
 def get_ducuments(url):
-     try: 
+     try:
         feed = feedparser.parse(url)
         feed_name=feed['feed']['title']
-        mydocs = db[feed_name].find()
+        mydocs = db[feed_name].find().limit(10) #{"date": {"$gt": datetime.datetime.today()}}
      except:
         print("Error while reading feed", feed_name)
      return mydocs
 
 #Connect to mongoDB
-client = pymongo.MongoClient("mongodb://rio:onslario89@riohomecloud.ddns.net:27017")
+client = pymongo.MongoClient("mongodb://rio:onslario89@localhost:27017")
 db = client.rss_news #database name
 
 start = date.today()
@@ -52,19 +53,26 @@ lines = f.readlines()
 contents=[]
 #Parsing feed rss
 for url in lines:
-    feed = feedparser.parse(url) 
-    feed_name=feed['feed']['title']
-    mydocs = db[feed_name].find({"date": {"$gt": datetime.datetime.today()}})     
-    if re.match(r'^TechCrunch', feed_name): 
-        for i in range(0, mydocs.count()):
-            contents.append([mydocs[i]['content'][0]['value'], mydocs[i]['link']])
-    else:
-        for i in range(0, mydocs.count()):
-            contents.append([mydocs[i]['summary'], mydocs[i]['link']]) 
-
+    try:
+        feed = feedparser.parse(url)
+        feed_name=feed['feed']['title']
+        print("found feed: ", feed_name)
+        mydocs = db[feed_name].find().limit(10) #{"date": {"$gt": datetime.datetime.today()}}
+        print("found: ",  mydocs.count(with_limit_and_skip=True), " feed")
+        if re.match(r'^TechCrunch', feed_name):
+            for i in range(0, mydocs.count(with_limit_and_skip=True)):
+                contents.append([mydocs[i]['content'][0]['value'], mydocs[i]['link']])
+        else:
+            for i in range(0, mydocs.count(with_limit_and_skip=True)):
+                contents.append([mydocs[i]['summary'], mydocs[i]['link']])
+    except Exception as e:
+        print(e)
+        pass
 for content in contents:
-    print(content[0])
-    if send_data_to_ai(content[0]) > 0.5:
-        send_message(content[1]) 
-        
-
+    try:
+        if send_data_to_ai(content[0]) > 0.5:
+            send_message(content[1])
+            print('sent to telegram..')
+    except Exception as e:
+        print(e)
+        pass
